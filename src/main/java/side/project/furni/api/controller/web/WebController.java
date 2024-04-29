0f63de41b640537f3299ac3ec176d098e72f4193
sariiiiiiii <abdf13423@naver.com>
2024-04-29 17:30:43 +0900

    modified web controller

--- src/main/java/side/project/furni/api/controller/web/WebController.java
+++ src/main/java/side/project/furni/api/controller/web/WebController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-    @GetMapping("/docs")
+    @GetMapping({"/", "/docs"})
     public String docs() {
         return "index.html";
     }
